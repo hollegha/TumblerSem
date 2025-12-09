a7e7046afb57d152d0852689b98631c477bdc3e4
hollegha <hl@bulme.at>
2025-12-09 22:27:20 +0100

    Add files via upload

---
+++ App/LineSensor/SvVis_Line.h
@@ -0,0 +1,37 @@
+
+// Achtung!! Keine Kommentare innerhalb der Sections
+// Com Schnittstelle 115200
+// Com:  COM4  500000
+
+IP: 192.168.4.1
+
+// Kanal Namen
+ChannelNames:
+	Ls1
+	Ls2
+	Ls3
+	Ls4
+	Ls5
+	Ls6
+	pos3
+	diff
+
+F_SAMPLE: 100
+
+// Y-Scales
+Y1Scale:  -2000 2000
+Y2Scale:  -300 300
+BarScale: -100 4000
+
+// Graphen ( Kurven )
+Graphs:
+	xxx Y1
+	xxx Y1
+	xxx Y1
+	xxx Y1
+	xxx Y1
+	xxx Y1
+	pos Y1
+	diff Y1
+end
+
